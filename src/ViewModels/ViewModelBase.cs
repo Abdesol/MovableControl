4693c35fe43d0700466efb6e6401a8dcf92c73b0
Abdella Solomon <abdesoltak@gmail.com>
2022-03-19 19:47:22 +0300

    Initial commit

---
+++ src/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace MovableControl.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ src/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MovableControl.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
