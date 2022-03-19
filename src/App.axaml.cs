cd4801089c298c60b37ebad502858d4317c74512
Abdella Solomon <abdesoltak@gmail.com>
2022-03-19 19:54:10 +0300

    Removed unnecessary files.

--- src/App.axaml.cs
+++ src/App.axaml.cs
@@ -1,7 +1,6 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-using MovableControl.ViewModels;
 using MovableControl.Views;
 
 namespace MovableControl
@@ -17,10 +16,7 @@ namespace MovableControl
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                desktop.MainWindow = new MainWindow
-                {
-                    DataContext = new MainWindowViewModel(),
-                };
+                desktop.MainWindow = new MainWindow();
             }
 
             base.OnFrameworkInitializationCompleted();
